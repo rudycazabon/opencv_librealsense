14f90323f53430d902167bacdd8c6e7adf1c3e9f
Rudy Cazabon <rudycazabon@users.noreply.github.com>
2016-09-13 14:01:44 -0400

    #undef CHRONO as it is not working properly

--- opencv_librealsense.cpp
+++ opencv_librealsense.cpp
@@ -1,7 +1,7 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2015 Intel Corporation. All Rights Reserved.
 
-#define CHRONO
+#undef CHRONO
 #define GLFW
 #define OPENCV
 
